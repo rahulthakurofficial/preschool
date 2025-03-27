6a07971e7adf4dbe3f75aacc8a0438ce2f8ba339
Rahul Kumar Thakur <rahulthakur.official6@gmail.com>
2025-03-27 14:40:54 +0530

    test

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -3,23 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
-import Testimonial from "./components/Testimonial/Testimonial";
+
 
 const App = () => {
   return (
-    // <Router>
-    //   <div className="app-container">
-    //     <Navbar/>
-    //     <div className="content">
-    //       <Routes>
-    //         <Route path="/" element={<Home />} /> 
-    //       </Routes>
-          
-    //     </div>
-    //     {/* <Testimonial/> */}
-    //     <Footer />
-    //   </div>
-    // </Router>
+   
     <div>
     <section id="Navbar">
       <Navbar />
@@ -30,18 +18,7 @@ const App = () => {
     <section id="Footer">
       <Footer />
     </section>
-    {/* <section id="Testimonial">
-      <Testimonial />
-    </section> */}
-    {/* <section id="Testimonial">
-      <Testimonial Reviews={Reviews} /> 
-    </section>
-    <section id="Feedback">
-      <FeedbackForm3D />
-    </section>
-    <section id="Footer">
-      <Footer />
-    </section> */}
+    
   </div>
   );
 };
